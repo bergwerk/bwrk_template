b5ad049d742d4492a9e11d127195743491b15d0d
Marvin Hübner <mjh@bergwerk.ag>
2015-09-02 15:20:08 +0200

    [task] bearbeiten des gulpfiles

--- gulpfile.js/config/sass.js
+++ gulpfile.js/config/sass.js
@@ -1,17 +1,6 @@
 var config = require('./');
 
 module.exports = {
-    autoprefixer: {
-        browsers: [
-            'last 2 version',
-            '> 1%',
-            'Firefox ESR'
-        ]
-    },
     src: config.assetDirectory + "/Css/**/*.{sass,scss}",
-    dest: config.publicDirectory + '/Css',
-    settings: {
-        precision: 10,
-        includePaths: [config.bowerDirectory]
-    }
+    dest: config.publicDirectory + '/Css'
 };
--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var errorHandler = require('../lib/handleErrors');
 var config = require('../config/sass');
 var gulpif = require('gulp-if');
 var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer-core');
 var csswring = require('csswring');
 
 gulp.task('sass', function () {
@@ -20,5 +18,4 @@ gulp.task('sass', function () {
         .pipe(gulpif(!production, sourcemaps.write()))
         .pipe(gulpif(production, postcss([csswring()])))
         .pipe(gulp.dest(config.dest))
-        .pipe(browserSync.reload({stream: true}));
 });
