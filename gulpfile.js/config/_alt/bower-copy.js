25356a3dfd3d61fcb8471029884494449110d360
Marvin Hübner <mjh@bergwerk.ag>
2015-09-02 15:21:29 +0200

    [task] löschen alter Gulp tasks

--- gulpfile.js/config/_alt/bower-copy.js
+++
@@ -1,8 +0,0 @@
-var config = require('./');
-
-module.exports = [
-    // {
-    //   src: config.bowerDirectory + '/foundation-icon-fonts/*.{eot,svg,ttf,otf,woff,wfoff2}',
-    //   dest: config.publicDirectory + '/Fonts/foundation-icons'
-    // }
-];
--- gulpfile.js/config/_alt/browserSync.js
+++
@@ -1,8 +0,0 @@
-var config = require('./');
-
-module.exports = {
-    server: false,
-    open: false,
-    proxy: config.browserSyncTarget,
-    files: [config.sourceDirectory + '/{Layouts,Partials,Templates}/**/*.html']
-};
