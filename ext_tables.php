3809e6325f1e7384cb5eba3b4691d7ab8aa0fa5a
Daniel Maier <dm@bergwerk.ag>
2015-11-16 10:19:52 +0100

    remove localconf and update tables

--- ext_tables.php
+++ ext_tables.php
@@ -6,10 +6,9 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     $_EXTKEY, 'Configuration/TypoScript',
-    'BERGWERK SitePackage'
+    'BERGWERK Template'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY .
-    '/Configuration/PageTS/setup.txt">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/setup.txt">'
 );
