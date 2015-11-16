3e884f40a764b4e74d05e1f404e2c453b65d3c15
Daniel Maier <dm@bergwerk.ag>
2015-11-16 10:25:08 +0100

    update

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'BERGWERK',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.99.99'
+			'typo3' => '6.2.0-7.6.99'
 		),
 		'conflicts' => array(),
 		'suggests' => array()
