4304a36021d7b7927e26ade718a2e6cb03a007ec
Daniel Maier <dm@bergwerk.ag>
2015-11-16 10:19:08 +0100

    2.0.0 alpha - refactor whole extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,17 @@
 <?php
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'BERGWERK Sitepackage',
+	'title' => 'BERGWERK Template',
 	'description' => '',
 	'category' => 'templates',
-	'version' => '1.1.1',
-	'state' => 'stable',
+	'version' => '2.0.0',
+	'state' => 'alpha',
 	'clearcacheonload' => 1,
 	'author' => 'BERGWERK [MJH]',
 	'author_email' => 'mjh@bergwerk.ag',
 	'author_company' => 'BERGWERK',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.99.99',
-			'css_styled_content' => '6.2.0-7.9.99',
-			'gridelements' => '3.2.0',
-			'bwrk_monsterkill' => '3.3.0',
-			'cooluri' => '1.1.1'
+			'typo3' => '6.2.0-7.99.99'
 		),
 		'conflicts' => array(),
 		'suggests' => array()
