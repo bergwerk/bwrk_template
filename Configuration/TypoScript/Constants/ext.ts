ad6cc688f91956f5e9e2a9ba864b9155dd7d4905
Daniel Maier <dm@bergwerk.ag>
2015-11-16 11:01:42 +0100

    use own templates

--- Configuration/TypoScript/Constants/ext.ts
+++ Configuration/TypoScript/Constants/ext.ts
@@ -35,3 +35,8 @@ EMAIL_CONTACT_TO_NAME =
 # tt_content
 content.defaultHeaderType = 3
 
+styles.templates {
+   templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/fluid_styled_content/Templates
+   partialRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/fluid_styled_content/Partials
+   layoutRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/fluid_styled_content/Layouts
+}
