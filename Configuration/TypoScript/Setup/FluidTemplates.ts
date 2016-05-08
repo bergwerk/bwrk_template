c7c02dae19c9e31c25f749cfde4ab43a7800bbe5
Marvin Huebner <mjh@bergwerk.ag>
2016-05-08 19:36:41 +0200

    include files

--- Configuration/TypoScript/Setup/FluidTemplates.ts
+++ Configuration/TypoScript/Setup/FluidTemplates.ts
@@ -1,23 +1,4 @@
 page {
-  10 = FLUIDTEMPLATE
-  10 {
-    file.stdWrap.cObject = CASE
-    file.stdWrap.cObject {
-      key.data = levelfield:-1, backend_layout_next_level, slide
-      key.override.field = backend_layout
-
-      default = TEXT
-      default.value = EXT:bwrk_template/Resources/Private/Fluid/Templates/Page/Default.html
-
-      pagets__2Columns = TEXT
-      pagets__2Columns.value = EXT:bwrk_template/Resources/Private/Fluid/Templates/Page/2Columns.html
-    }
-
-    templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Fluid/Templates/Page/
-    partialRootPath = EXT:bwrk_template/Resources/Private/Fluid/Partials/Page/
-    layoutRootPath = EXT:bwrk_template/Resources/Private/Fluid/Layouts/Page/
-  }
-
   headerData {
     10 = FLUIDTEMPLATE
     10 {
@@ -45,6 +26,25 @@ page {
       50 = TEXT
       50.value = </title>
     }
+  }
+
+  10 = FLUIDTEMPLATE
+  10 {
+    file.stdWrap.cObject = CASE
+    file.stdWrap.cObject {
+      key.data = levelfield:-1, backend_layout_next_level, slide
+      key.override.field = backend_layout
+
+      default = TEXT
+      default.value = EXT:bwrk_template/Resources/Private/Fluid/Templates/Page/Default.html
+
+      pagets__2Columns = TEXT
+      pagets__2Columns.value = EXT:bwrk_template/Resources/Private/Fluid/Templates/Page/2Columns.html
+    }
+
+    templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Fluid/Templates/Page/
+    partialRootPath = EXT:bwrk_template/Resources/Private/Fluid/Partials/Page/
+    layoutRootPath = EXT:bwrk_template/Resources/Private/Fluid/Layouts/Page/
   }
 
   footerData {
