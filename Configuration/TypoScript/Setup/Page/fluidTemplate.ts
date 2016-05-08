d7f6fa055bb9ad56157743971a0093cdc57c2dfe
Marvin Huebner <mjh@bergwerk.ag>
2016-05-08 18:42:03 +0200

    merge headerData und footerData in a single file

--- Configuration/TypoScript/Setup/Page/fluidTemplate.ts
+++ Configuration/TypoScript/Setup/Page/fluidTemplate.ts
@@ -17,5 +17,41 @@ page {
     partialRootPath = EXT:bwrk_template/Resources/Private/Fluid/Partials/Page/
     layoutRootPath = EXT:bwrk_template/Resources/Private/Fluid/Layouts/Page/
   }
-}
 
+  headerData {
+    10 = FLUIDTEMPLATE
+    10 {
+      file = EXT:bwrk_template/Resources/Private/Fluid/Templates/Header/Icons.html
+      templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Templates/Header/
+    }
+
+    15 >
+    15 = COA
+    15 {
+      10 = TEXT
+      10.value = <title>
+
+      20 = TEXT
+      20.wrap = |
+      20.data = leveltitle : 0
+
+      30 = TEXT
+      30.value = &nbsp;&middot;&nbsp;
+
+      40 = TEXT
+      40.field = subtitle // title
+      40.wrap = |
+
+      50 = TEXT
+      50.value = </title>
+    }
+  }
+
+  footerData {
+    10 = FLUIDTEMPLATE
+    10 {
+      file = EXT:bwrk_template/Resources/Private/Fluid/Templates/Footer/Footer.html
+      templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Templates/Footer/
+    }
+  }
+}
--- Configuration/TypoScript/Setup/Page/footerData.ts
+++
@@ -1,9 +0,0 @@
-page {
-  footerData {
-    10 = FLUIDTEMPLATE
-    10 {
-      file = EXT:bwrk_template/Resources/Private/Fluid/Templates/Footer/Footer.html
-      templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Templates/Footer/
-    }
-  }
-}
