890ca00bb0296a304f33690156438afe44599877
Marvin Huebner <mjh@bergwerk.ag>
2016-05-08 18:23:28 +0200

    Remove Powermail overwrites

--- Configuration/TypoScript/Ext/Powermail/setup.ts
+++
@@ -1,7 +0,0 @@
-plugin.tx_powermail {
-  view {
-    templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/Powermail/Templates/
-    partialRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/Powermail/Partials/
-    layoutRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/Powermail/Layouts/
-  }
-}
