7e0818911ffd0bf9634dafdb8f57b9d96f52e21b
Marvin Huebner <mjh@bergwerk.ag>
2016-05-08 18:22:58 +0200

    [TASK] remove nn_address overhead

--- Configuration/TypoScript/Ext/nn_address/setup.ts
+++
@@ -1,7 +0,0 @@
-plugin.tx_nnaddress {
-  view {
-    templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/nn_address/Templates/
-    partialRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/nn_address/Partials/
-    layoutRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/nn_address/Layouts/
-  }
-}
