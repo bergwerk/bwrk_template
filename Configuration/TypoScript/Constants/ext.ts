a8b60b48bdeb2068df73d38384a8956701829bea
Marvin Huebner <mjh@bergwerk.ag>
2016-05-08 18:33:25 +0200

    remove powermail and nn_address typoscript

--- Configuration/TypoScript/Constants/ext.ts
+++ Configuration/TypoScript/Constants/ext.ts
@@ -15,22 +15,6 @@ plugin.tx_news {
   }
 }
 
-# nn_address
-plugin.tx_nnaddress.settings.includeCSS = 0
-
-# powermail
-EMAIL_ADMIN_FROM_EMAIL =
-EMAIL_ADMIN_FROM_NAME =
-
-EMAIL_ADMIN_TO_EMAIL =
-EMAIL_ADMIN_TO_NAME =
-
-EMAIL_CONTACT_FROM_EMAIL =
-EMAIL_CONTACT_FROM_NAME =
-
-EMAIL_CONTACT_TO_EMAIL =
-EMAIL_CONTACT_TO_NAME =
-
 # tt_content
 content.defaultHeaderType = 3
 
