89df4055b659edfb373b2d83ebfcc26d94c66803
Marvin Huebner <mjh@bergwerk.ag>
2016-08-21 13:37:13 +0200

    [task] add new build process

---
+++ Build/example.gulp.localconf.js
@@ -0,0 +1,10 @@
+/**
+ * Copy this file and name it to gulp.localconf.js
+ *
+ * Set your project localhost here, to use browser-sync with a proxy
+ * @type {string}
+ */
+
+module.exports.localconf = {
+	localhost: ''
+};
---
+++ Build/gulp.paths.js
@@ -0,0 +1,7 @@
+const path = {
+	toSrc: '../src/',
+	toDist: '../public/',
+	toBower: '../src/bower_components/'
+};
+
+module.exports.path = path;
