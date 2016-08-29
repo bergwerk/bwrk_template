d566c198e9edd89d3e592a6b3f238b4c804fc6cc
Konstantin Sölch <ks@bergwerk.ag>
2016-08-29 09:57:45 +0200

    [TASK] adjust typoscript includes and news configuration

--- Configuration/TypoScript/Extensions/Constants/News.ts
+++ Configuration/TypoScript/Extensions/Constants/News.ts
@@ -1,12 +1,7 @@
-# News
 plugin.tx_news {
   view {
     templateRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/News/Templates/
     partialRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/News/Partials/
     layoutRootPath = EXT:bwrk_template/Resources/Private/Fluid/Ext/News/Layouts/
-  }
-
-  settings {
-    cssFile >
   }
 }
